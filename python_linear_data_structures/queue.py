fbd1b23d3cde4e8af0cbf12c118cb5bb8eb1da70
Alexander Bezobchuk <abezobchuk@gmail.com>
2016-05-17 17:57:49 -0400

    Update the queue implementation

--- python_linear_data_structures/queue.py
+++ python_linear_data_structures/queue.py
@@ -8,13 +8,17 @@ class Queue:
 
     self.__list = LinkedList()
 
+  def peek(self):
+    """Returns the node at front of the queue without removing it"""
+    return self.__list.get(0) if not self.is_empty() else None
+
   def dequeue(self):
     """Removes an item of the front of the queue (head)
 
     Time complexity: O(1)
     """
 
-    return self.__list.remove(0) if not self.empty() else None
+    return self.__list.remove(0) if not self.is_empty() else None
 
   def enqueue(self, data):
     """Pushes an item onto the back of the queue (tail)
@@ -32,13 +36,25 @@ class Queue:
 
     return self.__list.size
 
-  def empty(self):
+  def is_empty(self):
     """Determines if the queue is empty or not
 
     Time complexity: O(1)
     """
 
-    return self.__list.empty()
+    return self.__list.is_empty()
+
+  def clear(self):
+    """Clears out (empties) the queue.
+
+    Time complexity: O(1)
+    """
+
+    # Deallocate memory
+    del self.__list
+
+    # Create new linked list
+    self.__list = LinkedList()
 
   def pretty_print(self):
     """Pretty prints the queue in the following format:
